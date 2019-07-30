6aa13e5f766a9a6e80dabeb286d312766813ce8a
wuzuqiang <zuqiang_wu@skyseaok.com>
2019-07-30 13:50:22 +0800

    删除不必要的Nuget包

---
+++ EFQuick/TEST/UseMapperToMap/SourceProfile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UseMapperToMap
+{
+    //public class SourceProfile : Profile
+    //{
+    //    protected override void Configure()
+    //    {
+    //        CreateMap<Source, Destination>();
+    //    }
+    //}
+}
