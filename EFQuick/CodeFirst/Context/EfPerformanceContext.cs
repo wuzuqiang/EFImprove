6494db42e61a0d28088d66bc5805b239b525e34e
wuzuqiang <zuqiang_wu@skyseaok.com>
2021-04-03 14:49:06 +0800

    添加 参照天海的Fusion项目使用Logg Package来记录使用了哪些sql代码

--- EFQuick/CodeFirst/Context/EfPerformanceContext.cs
+++ EFQuick/CodeFirst/Context/EfPerformanceContext.cs
@@ -9,10 +9,22 @@ using System.Threading.Tasks;
 namespace CodeFirst.Context
 {
     public class EfPerformanceContext:DbContext
-    {
-        public EfPerformanceContext()
+	{
+		public static Common.Logging.ILog Log1 = Common.Logging.LogManager.GetLogger(typeof(EfPerformanceContext));
+
+		public EfPerformanceContext()
             :base("name=CodeFirstApp")
-        { }
+		{
+			this.Database.CommandTimeout = 600;
+			this.Database.Log = sql =>
+			{
+				if(!string.IsNullOrEmpty(sql))
+				{
+					//将处理过的sql写入日志
+					Log1.Info($"^_^^_^********sql：" + sql.ToString());
+				}
+			};
+		}
         public DbSet<Product> Products { get; set; }
 		public DbSet<OriginalOrder> OriginalOrders { get; set; }
 	}
